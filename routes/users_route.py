6c891d46a8c97e85b0b5a42b82cb7a174c70d44b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-12 15:49:21 +0300

    Updated the users route code

--- routes/users_route.py
+++ routes/users_route.py
@@ -1,61 +1,42 @@
-"""The Users route file"""
 from fastapi import APIRouter, HTTPException, status
+
 from models.users_model import User, UserSignIn
 
-user_router = APIRouter(prefix="/users", tags=["users"])
-
+user_router = APIRouter(
+    tags=["User"],
+)
 
 users = {}
 
 
-@user_router.post("/signup", status_code=status.HTTP_201_CREATED)
-async def sign_new_user(_data: User) -> dict:
-    """The endpoint to create a new user
-
-    Args:
-        _data (UserSignIn): The data used to create a new user
-
-    Raises:
-        HTTPException: A 409 is raised if t
-
-    Returns:
-        dict: _description_
-    """
-    if _data.email in users:
+@user_router.post("/signup")
+async def sign_user_up(data: User) -> dict:
+    if data.email in users:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
-            detail="User already exists",
+            detail="User with supplied username exists"
         )
 
-    users[_data.email] = _data
+    users[data.email] = data
 
-    return {"message": "User created successfully"}
+    return {
+        "message": "User successfully registered!"
+    }
 
 
 @user_router.post("/signin")
-async def sign_user_in(_user: UserSignIn) -> dict:
-    """The user sign in endpoint
-
-    Args:
-        _user (UserSignIn): The user data
-
-    Raises:
-        HTTPException: A 404 is raised if a user is not found
-        HTTPException: A 403 is raised if the user enters the wrong password
-
-    Returns:
-        dict: A message to show a successful signin
-    """
-    if users[_user.email] not in users:
+async def sign_user_in(user: UserSignIn) -> dict:
+    if user.email not in users:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail="User does not exist",
+            detail="User does not exist"
         )
 
-    if users[_user.email].password != _user.password:
+    if users[user.email].password != user.password:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
-            detail="Incorrect credentials",
+            detail="Wrong credential passed"
         )
-
-    return {"message": "User signed in successfully"}
+    return {
+        "message": "User signed in successfully"
+    }
