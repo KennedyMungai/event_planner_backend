e376c72b11e4f14a1f93aa1b17d9018c29038059
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-12 16:06:17 +0300

    Wrote the router to retrieve all events from the endpoint

--- routes/events_route.py
+++ routes/events_route.py
@@ -9,3 +9,9 @@ from models.events_model import Event
 event_router = APIRouter(prefix="/events", tags=["Events"])
 
 events = []
+
+
+@event_router.get("/", response_model=List[Event])
+async def retrieve_all_events() -> List[Event]:
+    """Retrieve all events"""
+    return events
