c966d86963cd35991b238dfddc45462a60f45dec
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-12 15:09:44 +0300

    Created a config subclass for teh Event model

--- models/users_model.py
+++ models/users_model.py
@@ -16,3 +16,15 @@ class Event(BaseModel):
     description: str
     tags: List[str]
     location: str
+
+    class Config:
+        """The config subclass for the Event model"""
+        schema_extra = {
+            "example": {
+                "title": "FastAPI Book Launch",
+                "image": "https://linktomyimage.com/image.png",
+                "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come with your own copy to win gifts!",
+                "tags": ["python", "fastapi", "book", "launch"],
+                "location": "Google Meet"
+            }
+        }
