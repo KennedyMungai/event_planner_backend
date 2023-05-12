f6ad067e35e59b76e36bfb31b33658a5893dffc8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-12 16:01:27 +0300

    Added a docstring to the user sign in endpoint

--- routes/users_route.py
+++ routes/users_route.py
@@ -11,6 +11,17 @@ users = {}
 
 @user_router.post("/signup")
 async def sign_user_up(data: User) -> dict:
+    """The endpoint for signing up a user
+
+    Args:
+        data (User): The user data
+
+    Raises:
+        HTTPException: A 409 is raised if the email already exists
+
+    Returns:
+        dict: A message to show a successful sign up
+    """
     if data.email in users:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
@@ -26,6 +37,18 @@ async def sign_user_up(data: User) -> dict:
 
 @user_router.post("/signin")
 async def sign_user_in(user: UserSignIn) -> dict:
+    """An endpoint to show a successful signin
+
+    Args:
+        user (UserSignIn): The user object
+
+    Raises:
+        HTTPException: A 404 is raised if the user is not found
+        HTTPException: A 403 is raised if the user enters the wrong password
+
+    Returns:
+        dict: _description_
+    """
     if user.email not in users:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
