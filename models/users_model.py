148904a993dcfe134e7d32fad3c84ec43d4b8fa5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-12 15:07:48 +0300

    Define dthe event model

--- models/users_model.py
+++ models/users_model.py
@@ -1,1 +1,18 @@
 """Users Model"""
+from typing import List
+
+from pydantic import BaseModel
+
+
+class Event(BaseModel):
+    """The event model
+
+    Args:
+        BaseModel (Pydantic Model): The model for the Event
+    """
+    id: int
+    title: str
+    image: str
+    description: str
+    tags: List[str]
+    location: str
