1f3b9f3c68ca8a96f7768c79f32dcf0eeea21653
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-12 14:54:26 +0300

    Created the root route for the application

--- app/main.py
+++ app/main.py
@@ -6,3 +6,9 @@ app = FastAPI(
     title="Event Planner Backend",
     description="A simple backend for an event planner"
 )
+
+
+@app.get("/", name="Root", description="The root route for testing",  tags=["Root"])
+async def root() -> dict[str, str]:
+    """The root route"""
+    return {"message": "Hello World"}
