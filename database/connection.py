028a74b0f52caf0050421aaeb3dee878989ab838
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-13 10:56:28 +0300

    Rewrote the script

--- database/connection.py
+++ database/connection.py
@@ -4,6 +4,14 @@ from typing import Optional
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 from pydantic import BaseSettings
+import os
+from dotenv import find_dotenv, load_dotenv
+
+
+load_dotenv(find_dotenv())
+
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 
 class Settings(BaseSettings):
@@ -12,13 +20,7 @@ class Settings(BaseSettings):
     Args:
         BaseSettings (pydantic): Pydantic Base Settings
     """
-    DATABASE_URL: Optional[str] = None
-
     async def initialize_database(self):
         """The function to initialize a database connection"""
         client = AsyncIOMotorClient(self.DATABASE_URL)
         await init_beanie(database=client.events, document_models=[])
-
-    class Config:
-        """The configuration subclass for the Settings class"""
-        env_file = "../.env"
