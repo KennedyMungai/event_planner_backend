fb4d72ab4e88631bd1250e5e03e7bc95a60de998
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-13 09:48:17 +0300

    IMported select from sqlmodel

--- routes/events_route.py
+++ routes/events_route.py
@@ -3,6 +3,7 @@ from typing import List
 from fastapi import APIRouter, Depends, HTTPException, Request, status
 from database.connection import get_session
 from models.events_model import Event, EventUpdate
+from sqlmodel import select
 
 
 events_router = APIRouter(prefix="/events", tags=["Events"])
