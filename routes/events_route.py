b08ac747903dbe087c5a1883aa4d1fc1b62c6fe9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-13 09:42:40 +0300

    Added more information for the api metadata thing

--- routes/events_route.py
+++ routes/events_route.py
@@ -7,7 +7,12 @@ from models.events_model import Event, EventUpdate
 events_router = APIRouter(prefix="/events", tags=["Events"])
 
 
-@events_router.post("/new")
+@events_router.post(
+    "/new",
+    name="Create Post",
+    description="The endpoint to create a new post",
+    status_code=status.HTTP_201_CREATED
+)
 async def create_event(new_event: Event, session=Depends(get_session)) -> dict[str, str]:
     """Creates a new event"""
     session.add(new_event)
