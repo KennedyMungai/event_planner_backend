b29bbbd89490dc8d0d1bd577fe5b9b5a8ad30278
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-13 11:14:24 +0300

    Created the User document class

--- models/users_model.py
+++ models/users_model.py
@@ -1,46 +1,25 @@
 """The model for the users data"""
-from typing import List, Optional
+from typing import Optional, List
+from beanie import Document, Link
 
 from pydantic import BaseModel, EmailStr
 
 from models.events_model import Event
 
 
-class User(BaseModel):
-    """The model for the user data
-
-    Args:
-        BaseModel (pydantic): The parent class for the User class
-    """
+class User(Document):
     email: EmailStr
     password: str
-    events: Optional[List[Event]]
+    events: Optional[List[Link[Event]]]
+
+    class Settings:
+        name = "users"
 
     class Config:
-        """The config for the User class"""
-        schema_extra = {
-            "example": {
-                "email": "kijana@mangaa .com",
-                "username": "strong!!!",
-                "events": [],
-            }
-        }
-
-
-class UserSignIn(BaseModel):
-    """The user sign in mode;
-
-    Args:
-        BaseModel (Pydantic): The parent class for the UserSignIn class
-    """
-    email: EmailStr
-    password: str
-
-    class Config:
-        """The config subclass"""
         schema_extra = {
             "example": {
                 "email": "kijana@mangaa.com",
-                "password": "strong!!!",
+                "password": "kijana123",
+                "events": []
             }
         }
