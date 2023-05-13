de49279357e5aca52bcd8a3a669fc7d4bc92f64f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-13 10:49:08 +0300

    Added a bunch of imports to the connection file

--- database/connection.py
+++ database/connection.py
@@ -1,20 +1,5 @@
 """The DB connection file"""
-from sqlmodel import Session, SQLModel, create_engine
-
-from models.events_model import Event
-
-DATABASE_CONNECTION_STRING = "sqlite:///planner.db"
-connect_args = {"check_same_thread": False}
-engine_url = create_engine(DATABASE_CONNECTION_STRING,
-                           connect_args=connect_args)
-
-
-def conn():
-    """The db connection function"""
-    SQLModel.metadata.create_all(engine_url)
-
-
-def get_session():
-    """The db session function"""
-    with Session(engine_url) as session:
-        yield session
+from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
+from typing import Optional
+from pydantic import BaseSettings
