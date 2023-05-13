653edc3934c03b1655943c1fc9efd695360a7a56
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-13 11:29:00 +0300

    Added the create function to the Database class

--- database/connection.py
+++ database/connection.py
@@ -1,8 +1,8 @@
 """The DB connection file"""
 import os
-from typing import Optional
+from typing import Any, Optional
 
-from beanie import init_beanie
+from beanie import init_beanie, PydanticObjectId
 from dotenv import find_dotenv, load_dotenv
 from motor.motor_asyncio import AsyncIOMotorClient
 from pydantic import BaseModel, BaseSettings
@@ -26,3 +26,12 @@ class Settings(BaseSettings):
         """The function to initialize a database connection"""
         client = AsyncIOMotorClient(DATABASE_URL)
         await init_beanie(database=client.events, document_models=[Event, User])
+
+
+class Database:
+    def __init__(self, model):
+        self.model = model
+
+    async def save(self, document) -> None:
+        await document.create()
+        return
