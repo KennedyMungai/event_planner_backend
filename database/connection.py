455a5733ea462136c41d52e32167d56b1df440f4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-13 10:52:25 +0300

    Created the Settings class that inherits BaseSerrings

--- database/connection.py
+++ database/connection.py
@@ -3,3 +3,14 @@ from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 from typing import Optional
 from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: Optional[str] = None
+
+    async def initialize_database(self):
+        client = AsyncIOMotorClient(self.DATABASE_URL)
+        await init_beanie(database=client.events, document_models=[])
+
+        class Config:
+            env_file = "../.env"
