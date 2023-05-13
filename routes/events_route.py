2e85358cf05513815db809f161f0c53eea87a56a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-13 09:32:18 +0300

    Created the endpoint for creating new events

--- routes/events_route.py
+++ routes/events_route.py
@@ -2,3 +2,16 @@
 from fastapi import APIRouter, Depends, HTTPException, Request, status
 from database.connection import get_session
 from models.events_model import Event, EventUpdate
+
+
+events_route = APIRouter(prefix="/events", tags=["Events"])
+
+
+@events_route.post("/new")
+async def create_event(new_event: Event, session=Depends(get_session)) -> dict[str, str]:
+    """Creates a new event"""
+    session.add(new_event)
+    session.commit()
+    session.refresh(new_event)
+
+    return {"message": "Event created successfully"}
