729aca2d0ea8683ba231d4bf9904aca461e1a9c4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-13 11:10:17 +0300

    Create dthe EventUpdate class as the model for the event update data

--- models/events_model.py
+++ models/events_model.py
@@ -2,6 +2,7 @@
 from typing import List, Optional
 
 from beanie import Document
+from pydantic import BaseModel
 
 
 class Event(Document):
@@ -33,3 +34,22 @@ class Event(Document):
         """The Settings subclass for the Event model
         """
         name = "events"
+
+
+class EventUpdate(BaseModel):
+    title: Optional[str]
+    image: Optional[str]
+    description: Optional[str]
+    tags: Optional[List[str]]
+    location: Optional[str]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "title": "Kijana Mang'aa",
+                "image": "https://www.kijana.com/wp-content/uploads/2019/12/Kijana-Mang'aa-2.jpg",
+                "description": "Kijana Mang'aa is a traditional Thai food event that is held in the city of Bangkok.",
+                "tags": ["Thai", "Food", "Bakery"],
+                "location": "Bangkok"
+            }
+        }
