a65ced356a40a0227cc3152a0f4230a00c2a9f62
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-13 11:21:21 +0300

    Added BaseModel to the pydantic imports

--- database/connection.py
+++ database/connection.py
@@ -5,7 +5,7 @@ from typing import Optional
 from beanie import init_beanie
 from dotenv import find_dotenv, load_dotenv
 from motor.motor_asyncio import AsyncIOMotorClient
-from pydantic import BaseSettings
+from pydantic import BaseSettings, BaseModel
 
 from models.events_model import Event
 from models.users_model import User
