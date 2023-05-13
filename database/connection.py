a68e4df68ded0c69aec3004901d2c5a6a7fdd7f4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-13 09:21:25 +0300

    Defined the db connection code

--- database/connection.py
+++ database/connection.py
@@ -1,1 +1,19 @@
 """The DB connection file"""
+from sqlmodel import SQLModel, Session, create_engine
+from models.events_model import Event
+
+
+database_file = "planner.db"
+database_connection_string = f"sqlite:///{database_file}"
+connect_args = {"check_same_thread": False}
+engine_url = create_engine(database_connection_string,
+                           connect_args=connect_args)
+
+
+def conn():
+    SQLModel.metadata.create_all(engine_url)
+
+
+def get_session():
+    with Session(engine_url) as session:
+        yield session
