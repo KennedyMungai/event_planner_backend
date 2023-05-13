233b25e25bafedc9c08b7f4f144c0486a6aedeae
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-13 09:22:40 +0300

    Added docstrings to theh functions in the python file

--- database/connection.py
+++ database/connection.py
@@ -3,17 +3,18 @@ from sqlmodel import SQLModel, Session, create_engine
 from models.events_model import Event
 
 
-database_file = "planner.db"
-database_connection_string = f"sqlite:///{database_file}"
+database_connection_string = "sqlite:///planner.db"
 connect_args = {"check_same_thread": False}
 engine_url = create_engine(database_connection_string,
                            connect_args=connect_args)
 
 
 def conn():
+    """The db connection function"""
     SQLModel.metadata.create_all(engine_url)
 
 
 def get_session():
+    """The db session function"""
     with Session(engine_url) as session:
         yield session
