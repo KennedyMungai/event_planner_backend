5c038416d7cc52c1103b160b0be815dfcde36422
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-13 10:53:56 +0300

    Added docstrings to the classes and functions inside the python file

--- database/connection.py
+++ database/connection.py
@@ -6,11 +6,18 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
+    """The settings class for the model
+
+    Args:
+        BaseSettings (pydantic): Pydantic Base Settings
+    """
     DATABASE_URL: Optional[str] = None
 
     async def initialize_database(self):
+        """The function to initialize a database connection"""
         client = AsyncIOMotorClient(self.DATABASE_URL)
         await init_beanie(database=client.events, document_models=[])
 
-        class Config:
-            env_file = "../.env"
+    class Config:
+        """The configuration subclass for the Settings class"""
+        env_file = "../.env"
